ad016dacfa30af617e2220f0db38acf8851fa7f2
814794087 <814794087@qq.com>
2018-07-06 08:00:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/human/handler/ProgressDialogActivity.java
@@ -0,0 +1,102 @@
+package com.example.human.handler;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+
+public class ProgressDialogActivity extends Activity {
+
+    private int[] data=new int[100];//这块的代码模拟了填充一个长度100的数组
+    int hasData=0;//记录对话框的完成百分比
+    final int PROGRESS_DIALOG=0x112;//定义对话框标识
+    int progressStatus=0;
+    ProgressDialog pd;
+    Handler handler;
+    @SuppressLint("HandlerLeak")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_dialog);
+        Button execBtn=(Button)findViewById(R.id.exec);
+        execBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(PROGRESS_DIALOG);
+            }
+        });
+        handler=new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                pd.setProgress (progressStatus);
+            }
+        };
+    }
+    @Override
+    public Dialog onCreateDialog(int id, Bundle status) {
+        System.out.println("------create------");
+        switch (id) {
+            case PROGRESS_DIALOG:
+                // 创建进度对话框
+                pd = new ProgressDialog(this);
+                pd.setMax(100);
+                // 设置对话框的标题
+                pd.setTitle("任务完成百分比");
+                // 设置对话框 显示的内容
+                pd.setMessage("耗时任务的完成百分比");
+                // 设置对话框不能用“取消”按钮关闭
+                pd.setCancelable(false);
+                // 设置对话框的进度条风格
+                pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                // 设置对话框的进度条是否显示进度
+                pd.setIndeterminate(false);
+                break;
+        }
+        return pd;
+    }
+    // 该方法将在onCreateDialog方法调用之后被回调
+    @Override
+    public void onPrepareDialog(int id, Dialog dialog) {
+        System.out.println("------prepare------");
+        super.onPrepareDialog(id, dialog);
+        switch (id) {
+            case PROGRESS_DIALOG:
+                // 对话框进度清零
+                pd.incrementProgressBy(-pd.getProgress());
+                new Thread() {
+                    @SuppressLint("HandlerLeak")
+                    public void run() {
+                        while (progressStatus < 100) {
+                            // 获取耗时操作的完成百分比
+                            progressStatus = doWork();
+                            Message msg=handler.obtainMessage();
+                            handler.sendMessage(msg);
+
+                        }
+                        // 如果任务已经完成
+                        if (progressStatus >= 100) {
+                            // 关闭对话框
+                            pd.dismiss();
+                        }
+                    }
+                }.start();
+                break;
+        }
+    }
+    // 模拟一个耗时的操作。
+    public int doWork() {
+        // 为数组元素赋值
+        data[hasData++] = (int) (Math.random() * 100);
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return hasData;
+    }
+}
